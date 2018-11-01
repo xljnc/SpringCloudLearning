0c787a1a93c80d1d31ff7f33d1f34b025b1c3708
wutian <wutian@zafh.com.cn>
2018-11-01 14:32:32 +0800

    project init

---
+++ src/main/java/com/wt/spring/cloud/SpringCloudLearningApplication.java
@@ -0,0 +1,12 @@
+package com.wt.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudLearningApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wt/spring/cloud/SpringCloudLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wt.spring.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudLearningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
