8581c61ea9699891a73b53fd5a1a2a347b38fdcd
wutian <wutian@zafh.com.cn>
2018-11-08 17:30:30 +0800

    SpringCloud学习工程 添加服务发现模块，基于eureka by WuTian 2018-Nov-08

--- discovery/src/main/java/com/wt/spring/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/wt/spring/discovery/DiscoveryApplication.java
@@ -2,9 +2,12 @@ package com.wt.spring.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
 @EnableEurekaServer
 public class DiscoveryApplication {
 
