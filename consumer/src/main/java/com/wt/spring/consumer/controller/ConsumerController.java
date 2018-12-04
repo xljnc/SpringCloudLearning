5c7fc4451a13c31ba61c857487f16df0518eedc6
wutian <wutian@zafh.com.cn>
2018-12-04 17:45:03 +0800

    SpringCloud学习工程 添加Hystrix by WuTian 2018-Nov-12

--- consumer/src/main/java/com/wt/spring/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/wt/spring/consumer/controller/ConsumerController.java
@@ -2,7 +2,6 @@ package com.wt.spring.consumer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
