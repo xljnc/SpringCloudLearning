7df6954a521c9fa12db5a05c505f55a81f78ce2f
qiyu <jsj0804wt@126.com>
2018-12-15 17:49:52 +0800

    添加Zuul模块 by Xljnc 2018-Dec-15

---
+++ zuul-proxy/src/test/java/com/wt/spring/zuulproxy/ZuulProxyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wt.spring.zuulproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulProxyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
