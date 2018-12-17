a6ba5a83dc4d98e966a4352e5eac12be4d60dbc4
qiyu <jsj0804wt@126.com>
2018-12-17 17:41:48 +0800

    zuul版本降为2.0.6 springboot 采用2.0.6 by Xljnc 2018-Dec-17

--- hystrix-dashboard/src/main/java/com/wt/spring/hystrix/dashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/wt/spring/hystrix/dashboard/HystrixDashboardApplication.java
@@ -2,9 +2,10 @@ package com.wt.spring.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableHystrixDashboard
 public class HystrixDashboardApplication {
 
